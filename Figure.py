be25358172b25b049b3b2e8f1faa54d229ff0d69
Ruslan <ilyin-r@mail.ru>
2022-12-25 18:55:03 +0300

    Optimize calculations

--- Figure.py
+++ Figure.py
@@ -13,12 +13,10 @@ class Figure:
         self.map = np.ones((self.width, self.depth, self.height), bool)
 
     def get_expanded_map(self, shape, position: tuple[int, int, int]):
-        width, depth, height = np.indices(shape)
-        shift_width, shift_depth, height_shift = position
-
-        width_map = (shift_width <= width) & (width < self.width + shift_width)
-        depth_map = (shift_depth <= depth) & (depth < self.depth + shift_depth)
-        height_map = (height_shift <= height) & (height < self.height + height_shift)
-
-        shift_map = width_map & depth_map & height_map
-        return np.ndarray(shape, bool, shift_map)
+        expanded_map = np.zeros(shape, bool)
+        expanded_map[
+        position[0]:position[0] + self.width,
+        position[1]:position[1] + self.depth,
+        position[2]:position[2] + self.height
+        ] = True
+        return expanded_map
