094f0e0faaf1000251089ebfbb2f6c48980ba107
Ruslan <ilyin-r@mail.ru>
2022-12-27 16:06:55 +0300

    Add two ways of visualization (#1)
    * Draft
    
    * Add two ways of visualization

--- Visualization.py
+++ Visualization.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
-import matplotlib.animation as animation
+import plotly.graph_objects as go
+import matplotlib.style as mplstyle
 
 
 def configure_plot(ax):
@@ -12,26 +13,62 @@ def configure_plot(ax):
 
 
 def show(data: np.ndarray):
+    matplotlib.use("TkAgg")
+    mplstyle.use('fast')
+    matplotlib.rcParams['path.simplify'] = True
+    matplotlib.rcParams['agg.path.chunksize'] = 10000
+    matplotlib.rcParams['path.simplify_threshold'] = 1.0
     ax = plt.figure().add_subplot(projection='3d')
     configure_plot(ax)
 
-    ax.voxels(data, edgecolors='#BFAB6E')
+    ax.voxels(data)
     plt.show()
 
 
-def show_gif(slices: list[np.ndarray], interval: int = 200):
-    def update_plot(num):
-        ax.clear()
-        configure_plot(ax)
-        ax.voxels(slices[num], edgecolors='#BFAB6E')
+x = [0, 0, 1, 1, 0, 0, 1, 1]
+y = [0, 1, 1, 0, 0, 1, 1, 0]
+z = [0, 0, 0, 0, 1, 1, 1, 1]
 
-    # Open gif in a modal window. Actual for PyCharm.
-    matplotlib.use("TkAgg")
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
+def show_by_3d_mesh(data: np.ndarray):
+    def create_mesh_cube_by_dot(dot):
+        x1, y1, z1 = dot
+        my3dmesh = go.Mesh3d(
+            x=list(np.asarray(x) + x1),
+            y=list(np.asarray(y) + y1),
+            z=list(np.asarray(z) + z1),
+            alphahull=0, intensity=np.linspace(1, 1, 8, endpoint=True), name='y')
+        return my3dmesh
 
-    ani = animation.FuncAnimation(fig, update_plot, interval=interval, frames=len(slices))
-    plt.show()
+    XYZ = np.argwhere(data)
+    fig = go.Figure()
+    for xyz in XYZ:
+        fig.add_trace(create_mesh_cube_by_dot(xyz))
+    fig.show()
 
-    return ani
+
+def show_by_3d_scatter(data: np.ndarray):
+    def create_point_cloud(dot):
+        x1, y1, z1 = dot
+        x, y, z = np.mgrid[x1:x1 + 0.9:8j, y1:y1 + 0.9:8j, z1:z1 + 0.9:8j]
+        return (x.flatten(), y.flatten(), z.flatten())
+
+    a = np.argwhere(data)
+    X = []
+    Y = []
+    Z = []
+    for l in a:
+        x, y, z = create_point_cloud(l)
+        X.extend(x)
+        Y.extend(y)
+        Z.extend(z)
+
+    marker_data = go.Scatter3d(
+        x=X,
+        y=Y,
+        z=Z,
+        marker=go.scatter3d.Marker(size=3),
+        mode='markers'
+    )
+    fig = go.Figure(data=marker_data)
+    fig.show()
